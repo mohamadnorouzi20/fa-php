7533eeb56abc6ec59978c3a3c55e1d65a5f3b9ff
Mohamad Norouzi <mohamadnorouzi20@outlook.com>
2020-11-28 20:36:52 +0330

    Upgraded pest and bumped version to 1.4

--- tests/Unit/FaDateTimeTest.php
+++ tests/Unit/FaDateTimeTest.php
@@ -6,29 +6,29 @@ use function MN\FaPhp\faDigits;
 it('can be created.', function () {
     $faDateTime = new FaDateTime();
 
-    assertInstanceOf(FaDateTime::class, $faDateTime);
+    $this->assertInstanceOf(FaDateTime::class, $faDateTime);
 });
 
 
 it('can instantiate an object with current datetime data.', function () {
-    assertInstanceOf(FaDateTime::class, FaDateTime::now());
+    $this->assertInstanceOf(FaDateTime::class, FaDateTime::now());
 });
 
 
 it('returns correct data for datetime accessors.', function () {
     $faDateTime = FaDateTime::createFromJalali(1373, 4, 23);
 
-    assertEquals('1373', $faDateTime->jYear());
-    assertEquals('4', $faDateTime->jMonth());
-    assertEquals('23', $faDateTime->jDay());
+    $this->assertEquals('1373', $faDateTime->jYear());
+    $this->assertEquals('4', $faDateTime->jMonth());
+    $this->assertEquals('23', $faDateTime->jDay());
 });
 
 
 it('can be formatted.', function () {
     $faDateTime = FaDateTime::createFromJalali(1373, 4, 5, 12, 8, 5);
 
-    assertEquals('1373/04/05 12:08:05', $faDateTime->format('jY/jm/jd H:i:s'));
-    assertEquals('73/4/5 12:08:05', $faDateTime->format('jy/jn/jj H:i:s'));
+    $this->assertEquals('1373/04/05 12:08:05', $faDateTime->format('jY/jm/jd H:i:s'));
+    $this->assertEquals('73/4/5 12:08:05', $faDateTime->format('jy/jn/jj H:i:s'));
 });
 
 
@@ -41,13 +41,13 @@ it('can format the weekdays.', function () {
     $faDateTime6 = FaDateTime::createFromJalali(1373, 4, 9);
     $faDateTime7 = FaDateTime::createFromJalali(1373, 4, 10);
 
-    assertEquals('شنبه', $faDateTime1->format('jl'));
-    assertEquals('یک شنبه', $faDateTime2->format('jl'));
-    assertEquals('دو شنبه', $faDateTime3->format('jl'));
-    assertEquals('سه شنبه', $faDateTime4->format('jl'));
-    assertEquals('چهار شنبه', $faDateTime5->format('jl'));
-    assertEquals('پنج شنبه', $faDateTime6->format('jl'));
-    assertEquals('جمعه', $faDateTime7->format('jl'));
+    $this->assertEquals('شنبه', $faDateTime1->format('jl'));
+    $this->assertEquals('یک شنبه', $faDateTime2->format('jl'));
+    $this->assertEquals('دو شنبه', $faDateTime3->format('jl'));
+    $this->assertEquals('سه شنبه', $faDateTime4->format('jl'));
+    $this->assertEquals('چهار شنبه', $faDateTime5->format('jl'));
+    $this->assertEquals('پنج شنبه', $faDateTime6->format('jl'));
+    $this->assertEquals('جمعه', $faDateTime7->format('jl'));
 });
 
 
@@ -65,18 +65,18 @@ it('can format the month names.', function () {
     $faDateTime11 = FaDateTime::createFromJalali(1373, 11, 1);
     $faDateTime12 = FaDateTime::createFromJalali(1373, 12, 1);
 
-    assertEquals('فروردین', $faDateTime1->format('jF'));
-    assertEquals('اردیبهشت', $faDateTime2->format('jF'));
-    assertEquals('خرداد', $faDateTime3->format('jF'));
-    assertEquals('تیر', $faDateTime4->format('jF'));
-    assertEquals('مرداد', $faDateTime5->format('jF'));
-    assertEquals('شهریور', $faDateTime6->format('jF'));
-    assertEquals('مهر', $faDateTime7->format('jF'));
-    assertEquals('آبان', $faDateTime8->format('jF'));
-    assertEquals('آذر', $faDateTime9->format('jF'));
-    assertEquals('دی', $faDateTime10->format('jF'));
-    assertEquals('بهمن', $faDateTime11->format('jF'));
-    assertEquals('اسفند', $faDateTime12->format('jF'));
+    $this->assertEquals('فروردین', $faDateTime1->format('jF'));
+    $this->assertEquals('اردیبهشت', $faDateTime2->format('jF'));
+    $this->assertEquals('خرداد', $faDateTime3->format('jF'));
+    $this->assertEquals('تیر', $faDateTime4->format('jF'));
+    $this->assertEquals('مرداد', $faDateTime5->format('jF'));
+    $this->assertEquals('شهریور', $faDateTime6->format('jF'));
+    $this->assertEquals('مهر', $faDateTime7->format('jF'));
+    $this->assertEquals('آبان', $faDateTime8->format('jF'));
+    $this->assertEquals('آذر', $faDateTime9->format('jF'));
+    $this->assertEquals('دی', $faDateTime10->format('jF'));
+    $this->assertEquals('بهمن', $faDateTime11->format('jF'));
+    $this->assertEquals('اسفند', $faDateTime12->format('jF'));
 });
 
 
@@ -85,7 +85,7 @@ it('can add an interval to the date', function () {
 
     $faDateTime->add(new DateInterval('P6Y4MT3M')); // 6 years and 4 months and 3 minutes
 
-    assertEquals('1379/8/24 12:11:35', (string) $faDateTime);
+    $this->assertEquals('1379/8/24 12:11:35', (string) $faDateTime);
 });
 
 
@@ -94,19 +94,19 @@ it('can subtract an interval from the date', function () {
 
     $faDateTime->sub(new DateInterval('P0Y7MT10S')); // 0 years and 7 months and 10 seconds
 
-    assertEquals('1372/9/23 12:08:25', (string) $faDateTime);
+    $this->assertEquals('1372/9/23 12:08:25', (string) $faDateTime);
 });
 
 
 it('can be formatted with farsi digits.', function () {
     $faDateTime = FaDateTime::createFromJalali(1373, 4, 23, 12, 8, 35);
 
-    assertEquals(faDigits('1373/04/23 12:08:35'), $faDateTime->format('jY/jm/jd H:i:s', true));
+    $this->assertEquals(faDigits('1373/04/23 12:08:35'), $faDateTime->format('jY/jm/jd H:i:s', true));
 });
 
 
 it('can be converted to a string.', function () {
     $faDateTime = FaDateTime::createFromJalali(1373, 4, 23, 12, 8, 35);
 
-    assertEquals('1373/4/23 12:08:35', (string) $faDateTime);
+    $this->assertEquals('1373/4/23 12:08:35', (string) $faDateTime);
 });
--- tests/Unit/FunctionsTest.php
+++ tests/Unit/FunctionsTest.php
@@ -7,36 +7,36 @@ use function MN\FaPhp\validateJalaliDate;
 use function MN\FaPhp\validateTime;
 
 it('can convert digits to farsi.', function () {
-    assertEquals('۰۱۲۳۴۵۶۷۸۹۰۱۲۳۴۵۶۷۸۹', faDigits('01234567890123456789'));
+    $this->assertEquals('۰۱۲۳۴۵۶۷۸۹۰۱۲۳۴۵۶۷۸۹', faDigits('01234567890123456789'));
 });
 
 it('can convert a gregorian date to jalali.', function () {
-    assertEquals([1373, 4, 23], gregorianToJalali(1994, 7, 14));
+    $this->assertEquals([1373, 4, 23], gregorianToJalali(1994, 7, 14));
 });
 
 it('can convert a jalali date to gregorian.', function () {
-    assertEquals([1994, 7, 14], jalaliToGregorian(1373, 4, 23));
+    $this->assertEquals([1994, 7, 14], jalaliToGregorian(1373, 4, 23));
 });
 
 it('can validate a jalali date', function () {
-    assertTrue(validateJalaliDate(1373, 4, 23));
-    assertTrue(validateJalaliDate(1373, 12, 29));
+    $this->assertTrue(validateJalaliDate(1373, 4, 23));
+    $this->assertTrue(validateJalaliDate(1373, 12, 29));
     
-    assertFalse(validateJalaliDate(1373, 12, 30)); // not a leap year
-    assertTrue(validateJalaliDate(1375, 12, 30)); // a leap year
+    $this->assertFalse(validateJalaliDate(1373, 12, 30)); // not a leap year
+    $this->assertTrue(validateJalaliDate(1375, 12, 30)); // a leap year
     
-    assertFalse(validateJalaliDate(0, 4, 23));
-    assertFalse(validateJalaliDate(1373, 0, 23));
-    assertFalse(validateJalaliDate(1373, 4, 0));
+    $this->assertFalse(validateJalaliDate(0, 4, 23));
+    $this->assertFalse(validateJalaliDate(1373, 0, 23));
+    $this->assertFalse(validateJalaliDate(1373, 4, 0));
 });
 
 it('can validate 24 hour format time', function () {
-    assertTrue(validateTime(0, 0, 0));
-    assertTrue(validateTime(23, 59, 59));
-    
-    assertFalse(validateTime(0, 0, -1));
-    assertFalse(validateTime(0, -1, 0));
-    assertFalse(validateTime(-1, 0, 0));
-    
-    assertFalse(validateTime(24, 0, 0));
+    $this->assertTrue(validateTime(0, 0, 0));
+    $this->assertTrue(validateTime(23, 59, 59));
+
+    $this->assertFalse(validateTime(0, 0, -1));
+    $this->assertFalse(validateTime(0, -1, 0));
+    $this->assertFalse(validateTime(-1, 0, 0));
+
+    $this->assertFalse(validateTime(24, 0, 0));
 });
